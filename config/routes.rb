b062edb276e716f34b60c752ddcaad151f477e13
David Bielik <davidbielik@gmail.com>
2022-06-13 15:51:17 -0400

    devise registration working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirects signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  
+  devise_for :users
   resources :friends
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "friends#index"
 end
